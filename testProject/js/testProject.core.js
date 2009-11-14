3e590909e8c6c192346ad4f0e5b49e908783a222
Morgan 'ARR\!' Allen <morganrallen@gmail.com>
2009-11-14 15:01:29 -0800

    Added build target to spawn new skeleton projects

---
+++ testProject/js/testProject.core.js
@@ -0,0 +1,15 @@
+(function(Backpack)
+{
+    Backpack.projectName = "testProject";
+
+    Backpack.on('running', function()
+    {
+    });
+    Backpack.on('status-ready', function()
+    {
+    });
+    Backpack.on('slider-ready', function()
+    {
+    });
+})(Backpack);
+        
---
+++ testProject/js/testProject.xml.js
@@ -0,0 +1,1 @@
+Backpack.testProject.xml = {};
